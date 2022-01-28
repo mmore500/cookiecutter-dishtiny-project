04c4c84895dc786901d44401aa8995da13a669a3
Matthew Andres Moreno <mmore500.login+gpg@gmail.com>
2022-01-28 12:45:57 -0500

    Remove unavailable Empirical features
    Haven't been merged back into DISHTINY's Empirical branch

--- {{cookiecutter.project_slug}}/cpp/source/web.cpp
+++ {{cookiecutter.project_slug}}/cpp/source/web.cpp
@@ -5,7 +5,6 @@
 #include <iostream>
 
 #include "Empirical/include/emp/prefab/ConfigPanel.hpp"
-#include "Empirical/include/emp/web/TextArea.hpp"
 #include "Empirical/include/emp/web/web.hpp"
 
 #include "{{cookiecutter.project_slug}}/config_setup.hpp"
@@ -25,21 +24,7 @@ int main() {
   setup_config_web(cfg);
   cfg.Write(std::cout);
   emp::prefab::ConfigPanel example_config_panel(cfg);
-  example_config_panel.ExcludeSetting("SUPER_SECRET");
-  example_config_panel.SetRange("SEED", "-1", "100", "1");
   doc << example_config_panel;
-
-  // An example to show how the Config Panel could be used
-  // to control the color of text in an HTML text area
-  UI::TextArea example_area("example_area");
-  example_area.SetSize(cfg.SIZE(), cfg.SIZE());
-  example_config_panel.SetOnChangeFun([](const std::string & setting, const std::string & value){
-    UI::TextArea example_area = doc.TextArea("example_area");
-    example_area.SetCSS("color", cfg.COLOR());
-    example_area.Redraw();
-  });
-
-  doc << example_area;
 
   std::cout << "Hello, console!" << "\n";
 
