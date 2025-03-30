504387df8a722994e1da1fe6f4827a5c895b4a1d
actions <actions-user@users.noreply.github.com>
2025-03-30 00:24:52 +0000

    Initial commit

---
+++ cpp/include/cut_dishtiny_project/example.hpp
@@ -0,0 +1,16 @@
+#pragma once
+#ifndef CUT_DISHTINY_PROJECT_EXAMPLE_HPP_INCLUDE
+#define CUT_DISHTINY_PROJECT_EXAMPLE_HPP_INCLUDE
+
+#include <iostream>
+
+namespace cut_dishtiny_project {
+
+bool example() {
+  std::cout << "example thing!" << '\n';
+  return true;
+}
+
+} // namespace cut_dishtiny_project
+
+#endif // #ifndef CUT_DISHTINY_PROJECT_EXAMPLE_HPP_INCLUDE
